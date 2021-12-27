4c7e17626b6d748be80612c1f2244fe8401b3c36
Anna-Lena Schwarzkopf <zckrbrt@gmail.com>
2021-12-27 23:27:35 +0100

    Add prettier and execute it

--- components/name-form.tsx
+++ components/name-form.tsx
@@ -1,42 +1,55 @@
-import React, { useEffect, useState } from "react"
-import { ReactSortable } from "react-sortablejs";
-import { useSessionStorage } from "../hooks/useSessionStorage";
+import React, { useEffect, useState } from 'react';
+import { ReactSortable } from 'react-sortablejs';
+import { useSessionStorage } from '../hooks/useSessionStorage';
 
-import { ItemType } from "../pages"
-
+import { ItemType } from '../pages';
 
 export const NameForm: React.FC = () => {
+  const [stateStored, setStateStored] = useSessionStorage('state', []);
+  const [inputStored, setInputStored] = useSessionStorage('input', '');
 
-    const [stateStored, setStateStored] = useSessionStorage("state", [])
-    const [inputStored, setInputStored] = useSessionStorage("input", "")
+  const addName = () => {
+    setStateStored([
+      ...stateStored,
+      { id: stateStored.length, name: inputStored }
+    ]);
+    setInputStored('');
+  };
 
-    const addName = () => {
-        setStateStored([...stateStored, { id: stateStored.length, name: inputStored }])
-        setInputStored('')
-    };
+  useEffect(() => {
+    setStateStored(stateStored);
+    setInputStored(inputStored);
+  });
 
-    useEffect(() => {
-        setStateStored(stateStored)
-        setInputStored(inputStored)
-    })
-
-    return (
-        <div className="text-blue-400">
-            <form onSubmit={e => {
-                e.preventDefault()
-                addName()
-            }} >
-                <label htmlFor="name" className="mr-2">Name</label>
-                <input value={inputStored} id="name" type="text" autoComplete="name" className="mr-2" required onChange={e => {
-                    setInputStored(e.target.value)
-                }} />
-                <button type="submit">Add</button>
-            </form>
-            <ReactSortable list={stateStored} setList={setStateStored}  >
-                {stateStored.map((item: ItemType) => (
-                    <div key={item.id}>{item.name}</div>
-                ))}
-            </ReactSortable>
-        </div>
-    )
-}
+  return (
+    <div className="text-blue-400">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addName();
+        }}
+      >
+        <label htmlFor="name" className="mr-2">
+          Name
+        </label>
+        <input
+          value={inputStored}
+          id="name"
+          type="text"
+          autoComplete="name"
+          className="mr-2"
+          required
+          onChange={(e) => {
+            setInputStored(e.target.value);
+          }}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ReactSortable list={stateStored} setList={setStateStored}>
+        {stateStored.map((item: ItemType) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
+      </ReactSortable>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import React from 'react'
-import { NameForm } from '../components/name-form'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import React from 'react';
+import { NameForm } from '../components/name-form';
 
 export interface ItemType {
   id: number;
@@ -9,33 +9,31 @@ export interface ItemType {
 }
 
 const Home: NextPage = () => {
-
   return (
-    <div className="bg-white dark:bg-gray-900 min-h-screen pr-1 flex flex-col justify-center items-center h-screen text-gray-900 dark:text-white" >
+    <div className="bg-white dark:bg-gray-900 min-h-screen pr-1 flex flex-col justify-center items-center h-screen text-gray-900 dark:text-white">
       <Head>
         <title>Your turn</title>
-        <meta name="description" content="Randomly choose a person or a thing out of a group" />
+        <meta
+          name="description"
+          content="Randomly choose a person or a thing out of a group"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="p-20 flex-1 flex-col justify-center items-center ">
-        <h1 className="hocus:underline text-4xl m-0 text-center mb-10" >
+        <h1 className="hocus:underline text-4xl m-0 text-center mb-10">
           Whose turn is it?
         </h1>
         <NameForm />
       </main>
 
       <footer className="w-full h-1/5 border-t-2 flex justify-center items-center flex-grow-1">
-        <a
-          href="https://lena.codes"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://lena.codes" target="_blank" rel="noopener noreferrer">
           Powered by @lena_codes
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
