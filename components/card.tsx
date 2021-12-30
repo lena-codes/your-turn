1e0699402e2bd973676acfb5690fecb923d09bc6
Anna-Lena Schwarzkopf <zckrbrt@gmail.com>
2021-12-30 17:07:50 +0100

    Create card container component

---
+++ components/card.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Card: React.FC = (props) => {
+  return (
+    <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      {props.children}
+    </div>
+  );
+};
